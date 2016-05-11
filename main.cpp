af523016ed16c6f7aaedd6c7d7740f3687b5f8f5
John Washam <john@readyprompt.com>
2016-05-11 15:39:06 -0700

    Experiments with cout, math, random, cin, data type sizes, escaping.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,100 @@
 #include <iostream>
+#include <cmath>
+#include <climits>
 
 using namespace std;
 
+void doMath();
+void testCout();
+void showRandom();
+int getAge();
+void printAge(int age);
+void showTypeSizes();
+void funWithEscaping();
+
 int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+
+    testCout();
+    doMath();
+    showRandom();
+//    int age = getAge();
+//    printAge(age);
+    showTypeSizes();
+//    funWithEscaping();
+
+    return EXIT_SUCCESS;
+}
+
+void testCout()
+{
+    int carrots = 25;
+
+    cout << "Hello, World! I have " << carrots << " carrots!" << endl;
+
+    cout << "Ate one, now " << --carrots << " carrots." <<endl;
+}
+
+void doMath()
+{
+
+    double target = 6.25;
+    double root = sqrt(target);
+
+    cout << "The square root of " << target << " is: " << root << endl;
+
+    double base = 5;
+    double exp = 8;
+    double powered = pow(base, exp);
+
+    cout << "The base " << base << " raised to the power of " << exp << " is " << powered << endl;
+}
+
+void showRandom()
+{
+    cout << "Here's a random integer: " << rand() << endl;
+}
+
+int getAge()
+{
+
+    int age;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    return age;
+}
+
+void printAge(int age)
+{
+    cout << "Wow, " << age << ", huh? That's " << age * 12 <<  " months." << endl;
+
+//    cout << "and age is stored here: " << &age << endl;
+}
+
+void showTypeSizes()
+{
+    int int_size = sizeof(int);
+    int byte_size = CHAR_BIT;
+    int max_int = INT_MAX;
+    short max_short = SHRT_MAX;
+    long max_long = LONG_MAX;
+    long long max_llong = LLONG_MAX;
+
+    cout << "int in bytes: " << int_size << endl;
+    cout << "bits to a char: " << byte_size << endl;
+    cout << "max int: " << max_int << endl;
+    cout << "max short: " << max_short << endl;
+    cout << "max long: " << max_long << endl;
+    cout << "max long long: " << max_llong << endl;
+}
+
+void funWithEscaping()
+{
+    cout << "Enter a number: ________\b\b\b\b\b\b\b\b";  // back up on the underscores
+
+    int secretNumber;
+    cin >> secretNumber;
+
+    cout << "Verified." << endl;
 }
